cf0ce881037c917c79bff4bd64427bd005489fe4
Edmond <sargsyan-norik@inbox.ru>
2022-09-17 09:30:24 +0400

    Object Tracker
    OpenCV Object Tracking Implementation In Python

--- ObjectTracker.py
+++ ObjectTracker.py
@@ -1,7 +1,6 @@
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture('Cars.mp4')
+cap = cv2.VideoCapture('cars.mp4')
 
 OPENCV_OBJECT_TRACKERS = {
     "csrt": cv2.legacy.TrackerCSRT_create,
@@ -22,29 +21,30 @@ while True:
     frame = cv2.resize(frame, (1080, 600))
 
     (success, boxes) = trackers.update(frame)
-    # print(success,boxes)
+    print(success, boxes)
     if success == False:
         bound_boxes = trackers.getObjects()
-        idx = np.where(bound_boxes.sum(axis=1) != 0)[0]
-        bound_boxes = bound_boxes[idx]
-        trackers = cv2.legacy.MultiTracker_create()
+        # idx = np.where(bound_boxes.sum(axis=5) != 0)[0]
+        # bound_boxes = bound_boxes[idx]
         for bound_box in bound_boxes:
             trackers.add(tracker, frame, bound_box)
 
     for i, box in enumerate(boxes):
         (x, y, w, h) = [int(v) for v in box]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 5, 250), 2)
-        cv2.putText(frame, 'TRACKING', (x + 10, y - 3), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 10, 255), 2)
+        cv2.putText(frame, 'TRACKING', (x + 20, y - 3), cv2.FONT_HERSHEY_PLAIN, 1.5, (25, 255, 0), 2)
 
     cv2.imshow('Following', frame)
-    k = cv2.waitKey(30)
+    k = cv2.waitKey(40)
 
-    if k == ord(" "):  # press
-        roi = cv2.selectROI("Frame", frame, fromCenter=False,
+    if k == ord(" "):
+        Roi = cv2.selectROI('Following', frame, fromCenter=False,
                             showCrosshair=True)
 
         tracker = OPENCV_OBJECT_TRACKERS['kcf']()
-        trackers.add(tracker, frame, roi)
+        trackers.add(tracker, frame, Roi)
+    if k == ord("q"):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
